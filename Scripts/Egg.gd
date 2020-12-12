f50b44272065ec4669d5d43fc4449e9ae8fd739b
Bryan <bryan87reed@gmail.com>
2020-12-12 14:58:06 -0700

    Creates egg scene and counts nurture clicks

--- Scripts/Egg.gd
+++ Scripts/Egg.gd
@@ -4,15 +4,26 @@ signal nurture_pressed
 
 var particle = preload("res://Scenes/ParticleIcon.tscn")
 
-# Called when the node enters the scene tree for the first time.
+var nurture_count_dict = {
+	"Dark": 0,
+	"Cold": 0,
+	"Hate": 0,
+	"Love": 0,
+	"Crystal": 0,
+	"Heat": 0,
+	"Light": 0,
+	"Slime": 0
+}
+
 func _ready():
 	connect("nurture_pressed", self, "_on_nurture_pressed")
 
 func _on_nurture_pressed(type):
+	show_nurture_particle(type)
+	nurture_count_dict[type] += 1
+	print(nurture_count_dict)
+	
+func show_nurture_particle(type):
 	var new_particle = particle.instance()
 	new_particle.icon_type = type
 	$EggSprite/ParticleStart.add_child(new_particle)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
