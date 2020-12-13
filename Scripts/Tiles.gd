c75ad26e358a8a907026c6e83e300871e05d4352
Ross <ross.srk@gmail.com>
2020-12-13 02:30:40 -0800

    Adds spikes to procedural cave generation

--- Scripts/Tiles.gd
+++ Scripts/Tiles.gd
@@ -1,24 +1,26 @@
 extends Node2D
 
-export var map_size: Vector2 = Vector2(500,28)
+export var map_size: Vector2 = Vector2(500,34)
 export var spawn_width: int = 10
 export var strip_min: int = 3
 export var strip_max: int = 30
 export var ceiling_min: int = 2
-export var ceiling_max: int = 6
-export var ground_min: int = 10
-export var ground_max: int = 17
+export var ceiling_max: int = 8
+export var ground_min: int = 15
+export var ground_max: int = 22
 
-enum TILE {NONE, GROUND, WATER, LAVA, C_SPIKE, F_SPIKE = -1}
+enum TILE {NONE, GROUND, WATER, LAVA, SPIKE_UP, SPIKE_DOWN, SPIKE_WATER = -1}
 var matrix = []
 var ceiling_line = []
 var floor_line = []
+var water_tops = []
 var water_bottoms = []
 var lava_line = []
 var tile_ground = preload("res://Scenes/TileGround.tscn")
 var tile_water = preload("res://Scenes/TileWater.tscn")
 var tile_lava = preload("res://Scenes/TileLava.tscn")
 var tile_spike = preload("res://Scenes/TileSpike.tscn")
+var tile_submerged_spike = preload("res://Scenes/TileSubmergedSpike.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -36,6 +38,7 @@ func generate_map():
 func prepare_matrix():
 	for x in range(map_size.x + 1):
 		matrix.append([])
+		water_tops.append(int(0))
 		water_bottoms.append(int(0))
 		for y in range(map_size.y + 1):
 			matrix[x].append(TILE.NONE)
@@ -54,6 +57,11 @@ func add_floor():
 				strip_buffer = rand_int(strip_min, strip_max)
 				current_height = rand_int(ground_min, ground_max)
 			floor_line.append(current_height)
+			# 1 in 3 chance to add a spike here if a valid spot
+			if rand_int(1,3) == 1:
+				if can_spike_go_here(Vector2(x,current_height - 1)):
+					add_spike(Vector2(x,current_height - 1),Vector2.UP,false)
+			# fill in rest of floor with ground tiles
 			for y in range(current_height,map_size.y + 1):
 				matrix[x][y] = TILE.GROUND
 			strip_buffer -= 1
@@ -72,6 +80,9 @@ func add_ceiling():
 			strip_buffer = rand_int(strip_min, strip_max)
 			current_height = rand_int(ceiling_min, ceiling_max)
 		ceiling_line.append(current_height)
+		# 1 in 3 chance to add a spike here
+		if rand_int(1,3) == 1:
+			add_spike(Vector2(x,current_height + 1),Vector2.DOWN,false)
 		for y in range(0, current_height + 1):
 			matrix[x][y] = TILE.GROUND
 		strip_buffer -= 1
@@ -94,9 +105,15 @@ func add_water():
 				var water_depth = rand_int(strip_height + 4, strip_height + 8)
 				var start_padding = rand_int(1, strip_len - water_len - 1)
 				for i in range(strip_start + start_padding, strip_start + water_len + 1):
+					water_tops[i] = strip_height
 					water_bottoms[i] = water_depth
+					# cleanup any spikes floating above where this new water is
+					if matrix[i][strip_height - 1] == TILE.SPIKE_UP:
+						matrix[i][strip_height - 1] = TILE.NONE
 					for j in range(strip_height, water_depth + 1):
 						matrix[i][j] = TILE.WATER
+					# add submerged spike to bottom of water
+					add_spike(Vector2(i,water_depth),Vector2.UP,true)
 			strip_start = x
 			strip_height = floor_line[x]
 
@@ -127,6 +144,27 @@ func add_lava():
 			matrix[x][y] = TILE.LAVA
 		strip_buffer -= 1
 
+func can_spike_go_here(coord: Vector2):
+	# flag invalid if too close to existing spike
+	for i in range(1,3):
+		if coord.x - i > 0:
+			if matrix[coord.x - i][coord.y] == TILE.SPIKE_UP:
+				return false
+		if coord.x + i < map_size.x:
+			if matrix[coord.x + i][coord.y] == TILE.SPIKE_UP:
+				return false
+	#  or if first tile after climb
+	if coord.x > 0 and floor_line[coord.x - 1] - (coord.y + 1) > 1:
+		return false
+	return true
+
+func add_spike(coord: Vector2, facing: Vector2, submerged: bool):
+	match facing:
+		Vector2.UP:
+			matrix[coord.x][coord.y] = TILE.SPIKE_WATER if submerged else TILE.SPIKE_UP
+		Vector2.DOWN:
+			matrix[coord.x][coord.y] = TILE.SPIKE_WATER if submerged else TILE.SPIKE_DOWN
+
 func render_matrix():
 	for x in range(map_size.x + 1):
 		for y in range(map_size.y + 1):
@@ -138,6 +176,13 @@ func render_matrix():
 					t = tile_water.instance()
 				TILE.LAVA:
 					t = tile_lava.instance()
+				TILE.SPIKE_UP:
+					t = tile_spike.instance()
+				TILE.SPIKE_DOWN:
+					t = tile_spike.instance()
+					t.flip_upside_down()
+				TILE.SPIKE_WATER:
+					t = tile_submerged_spike.instance()
 			if t:
 				t.position = Vector2(x * 16, y * 16)
 				add_child(t)
