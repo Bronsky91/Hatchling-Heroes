b2ef3c88cd00858572724626998137fea3df75df
Ross <ross.srk@gmail.com>
2020-12-15 23:33:45 -0800

    Adds surprise(!) enemy to WIP enemy spawner

--- Scripts/EnemySpawner.gd
+++ Scripts/EnemySpawner.gd
@@ -3,9 +3,10 @@ extends Node2D
 onready var player = get_node("../Player")
 var rat = preload("res://Scenes/Rat.tscn")
 var bat = preload("res://Scenes/Bat.tscn")
+var batrat = preload("res://Scenes/BatRat.tscn")
 
 func _on_Timer_timeout():
-	var i = bat.instance()
+	var i = bat.instance() if g.rand_int(1,2) == 1 else batrat.instance()
 	i.position = player.position
 	i.position.x += 400
 	i.position.y += g.rand_int(-64, 64)
